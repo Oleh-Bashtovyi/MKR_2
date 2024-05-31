98155e7e25ebfb9d47737fe90139bb7a2f16f53a
Oleh <olehbashtovyi@gmail.com>
2024-05-31 11:39:32 +0300

    add fix for tests in workflow

--- project_gallery/settings.py
+++ project_gallery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3)rjgz!)8hpqk#-3gua@da_qdvgg=t^#t)ftx-9v4n+@id#ca(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
